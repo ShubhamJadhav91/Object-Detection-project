05a5e3a29c2cafaaaae3efbe150f4cb5b8e1e670
ShubhamJadhav91 <shubhamj8677@gmail.com>
2025-01-13 21:26:07 +0530

    Add files via upload

---
+++ gui_main.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 14:27:05 2021
+
+@author: om
+"""
+
+import tkinter as tk
+from tkinter import ttk, LEFT, END
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox as ms
+import cv2
+import sqlite3
+import os
+import numpy as np
+import time
+from tkvideo import tkvideo
+#import video_capture as value
+#import lecture_details as detail_data
+#import video_second as video1
+#import lecture_video  as video
+
+global fn
+fn = ""
+##############################################+=============================================================
+root = tk.Tk()
+root.configure(background="brown")
+# root.geometry("1300x700")
+
+
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry("%dx%d+0+0" % (w, h))
+root.title("Object Detection and display Count Using Machine Learning")
+
+# 43
+
+image2 = Image.open('face.jpg')
+image2 = image2.resize((1530, 700), Image.ANTIALIAS)
+
+background_image = ImageTk.PhotoImage(image2)
+
+background_label = tk.Label(root, image=background_image)
+
+background_label.image = background_image
+
+background_label.place(x=0, y=0)  # , relwidth=1, relheight=1)
+label_l1 = tk.Label(root, text="Object Detection and display Count Using Machine Learning",font=("Times New Roman", 30, 'bold'),
+                    background="white", fg="Red", width=50, height=1)
+label_l1.place(x=30, y=0)
+
+#T1.tag_configure("center", justify='center')
+#T1.tag_add("center", 1.0, "end")
+
+################################$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#def clear_img():
+#    img11 = tk.Label(root, background='bisque2')
+#    img11.place(x=0, y=0)
+
+
+#################################################################$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+################################$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# def cap_video():
+    
+#     video1.upload()
+#     #from subprocess import call
+#     #call(['python','video_second.py'])
+
+def reg():
+    from subprocess import call
+    call(["python","Registration.py"])
+
+def log():
+    from subprocess import call
+    call(["python","Login.py"])
+    
+def window():
+  root.destroy()
+
+
+button1 = tk.Button(root, text="Login", command=log, width=12, height=1,font=('times', 20, ' bold '), bg="blue", fg="white")
+button1.place(x=1150, y=80)
+
+button2 = tk.Button(root, text="Sign-Up",command=reg,width=12, height=1,font=('times', 20, ' bold '), bg="green", fg="white")
+button2.place(x=1150, y=140)
+
+button3 = tk.Button(root, text="Exit",command=window,width=12, height=1,font=('times', 20, ' bold '), bg="red", fg="white")
+button3.place(x=1150, y=200)
+
+root.mainloop()
